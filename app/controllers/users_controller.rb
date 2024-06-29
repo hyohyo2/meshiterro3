45b20b704cc2343fe4922e56bc9882921f8687e7
hyohyo2 <hono.handball.0910@gmail.com>
2024-06-29 10:41:22 +0000

    [Fix]loginレイアウト

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,11 +6,12 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+    
   end
 
   def update
-    user = User.find(params[:id])
-    user.update(user_params)
+    @user = User.find(params[:id])
+    @user.update(user_params)
     redirect_to user_path(user)
   end
 
